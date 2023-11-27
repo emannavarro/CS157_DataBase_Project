810a93a8ff78fff0a12621b7828a20b447729219
Emanuel Navarro-Ortiz <emannavarro@gmail.com>
2023-11-26 19:35:43 -0800

    Added shop table outline

--- databasefrontend/src/pages/shop.js
+++ databasefrontend/src/pages/shop.js
@@ -30,16 +30,57 @@ class shop extends React.Component{
 			</header>
 
 			<div id="wrapper">
-
-		
 					<section id="main" class="wrapper">
 						<div class="inner">
 							<h1 class="major">Shopping</h1>
+									<div class="table-wrapper">
+										<table>
+											<thead>
+												<tr>
+													<th>Name</th>
+													<th>Description</th>
+													<th>Price</th>
+												</tr>
+											</thead>
+											<tbody>
+												<tr>
+													<td>Item One</td>
+													<td>Ante turpis integer aliquet porttitor.</td>
+													<td>29.99</td>
+												</tr>
+												<tr>
+													<td>Item Two</td>
+													<td>Vis ac commodo adipiscing arcu aliquet.</td>
+													<td>19.99</td>
+												</tr>
+												<tr>
+													<td>Item Three</td>
+													<td> Morbi faucibus arcu accumsan lorem.</td>
+													<td>29.99</td>
+												</tr>
+												<tr>
+													<td>Item Four</td>
+													<td>Vitae integer tempus condimentum.</td>
+													<td>19.99</td>
+												</tr>
+												<tr>
+													<td>Item Five</td>
+													<td>Ante turpis integer aliquet porttitor.</td>
+													<td>29.99</td>
+												</tr>
+											</tbody>
+										</table>
+									</div>
 
 						</div>
+
+						
 					</section>
 
+
 			</div>
+
+			
 
 
 			<footer id="footer" class="wrapper alt">
